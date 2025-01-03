64c508835d6bd84ae7a75ea83c694c21ef88724d
Alessandro <galloalessandro77@gmail.com>
2025-01-03 20:18:14 +0100

    Removed contact and about

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/home-page/HomePage.jsx";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
-    },
-    {
-      path: "about",
-      element: <About />,
-    },
-    {
-      path: "contact",
-      element: <Contact />,
     },
   ]);
 
